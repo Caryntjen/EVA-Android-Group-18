666a9c603825d3e9241aa714d89be2741859f33b
Matthias <matthias.caryn.n2702@student.hogent.be>
2015-10-27 01:26:11 +0100

    Created Challenge & Register Activity and linked them to the Login Activity + First fragment

--- app/src/main/java/com/evavzw/twentyonedayschallenge/LoginActivity.java
+++ app/src/main/java/com/evavzw/twentyonedayschallenge/LoginActivity.java
@@ -1,16 +1,14 @@
 package com.evavzw.twentyonedayschallenge;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 /**
  * A login screen that offers login or register via email/password.
@@ -32,20 +30,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         // Set up the login form.
         mEmailView = (EditText) findViewById(R.id.email);
-
         mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
-                    return true;
-                }
-                return false;
-            }
-        });
 
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
@@ -65,8 +53,6 @@ public class LoginActivity extends AppCompatActivity {
 
         mLoginFormView = findViewById(R.id.login_form);
     }
-
-
 
     /**
      * Attempts Register an account.
@@ -117,67 +103,21 @@ public class LoginActivity extends AppCompatActivity {
             focusView.requestFocus();
         } else {
             // Kick off a background task to perform the user login attempt.
+            Intent i = null;
             if(stask.equalsIgnoreCase("register"))
+            {
                 task = new UserRegisterTask(email, password);
-                  else  task = new UserLoginTask(email, password);
+                i = new Intent(this, RegisterActivity.class);
+            }
+            else {
+                task = new UserLoginTask(email, password);
+                i = new Intent(this, ChallengesActivity.class);
+            }
+
             task.execute((Void) null);
+            startActivity(i);
         }
 
-    }
-
-    /**
-     * Attempts to sign in the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
-    private void attemptLogin() {
-        if (mAuthLoginTaks != null) {
-            return;
-        }
-
-        // Reset errors.
-        mEmailView.setError(null);
-        mPasswordView.setError(null);
-
-        // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
-
-        boolean cancel = false;
-        View focusView = null;
-
-        // Check for a valid password, if the user entered one.
-        if (TextUtils.isEmpty(password)) {
-            mPasswordView.setError(getString(R.string.error_field_required));
-            focusView = mPasswordView;
-            cancel = true;
-        } else if (!isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
-            cancel = true;
-        }
-
-        // Check for a valid email address.
-        if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
-            cancel = true;
-        } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
-            cancel = true;
-        }
-
-        if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-            focusView.requestFocus();
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            mAuthLoginTaks = new UserLoginTask(email, password);
-            mAuthLoginTaks.execute((Void) null);
-        }
     }
 
     private boolean isEmailValid(String email) {
